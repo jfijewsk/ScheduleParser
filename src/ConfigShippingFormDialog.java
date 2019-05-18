5fb3ed734d70e309694ce616a835c6b7ee926bb5
James Fijewski <jfijewsk@asu.edu>
2019-05-17 21:41:25 -0500

    Ending the night

--- src/ConfigShippingFormDialog.java
+++ src/ConfigShippingFormDialog.java
@@ -27,6 +27,9 @@ public class ConfigShippingFormDialog extends javax.swing.JDialog {
         shippingFormBrowseBtn = new javax.swing.JButton();
         cancelBtn = new javax.swing.JButton();
         saveBtn = new javax.swing.JButton();
+        rootSaveFolderJlabel = new javax.swing.JLabel();
+        rootSaveFolderTextField = new javax.swing.JTextField();
+        rootSaveFolderBrowseBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -55,24 +58,43 @@ public class ConfigShippingFormDialog extends javax.swing.JDialog {
             }
         });
 
+        rootSaveFolderJlabel.setText("Root Save Folder:");
+
+        rootSaveFolderTextField.setText("jTextField1");
+
+        rootSaveFolderBrowseBtn.setText("Browse");
+        rootSaveFolderBrowseBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rootSaveFolderBrowseBtnActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(shippingFormJlabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(shippingFormTextField)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(shippingFormBrowseBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(22, 22, 22))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(303, Short.MAX_VALUE)
                 .addComponent(saveBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(65, 65, 65)
                 .addComponent(cancelBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(298, 298, 298))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(shippingFormJlabel)
+                    .addComponent(rootSaveFolderJlabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(shippingFormTextField)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(shippingFormBrowseBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(rootSaveFolderTextField)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(rootSaveFolderBrowseBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -84,9 +106,14 @@ public class ConfigShippingFormDialog extends javax.swing.JDialog {
                     .addComponent(shippingFormBrowseBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rootSaveFolderJlabel, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(rootSaveFolderTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(rootSaveFolderBrowseBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(saveBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(cancelBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
@@ -104,13 +131,19 @@ public class ConfigShippingFormDialog extends javax.swing.JDialog {
         // TODO add your handling code here:
     }                                       
 
+    private void rootSaveFolderBrowseBtnActionPerformed(java.awt.event.ActionEvent evt) {                                                        
+        // TODO add your handling code here:
+    }                                                       
+
 
     // Variables declaration - do not modify                     
     private javax.swing.JButton cancelBtn;
+    private javax.swing.JButton rootSaveFolderBrowseBtn;
+    private javax.swing.JLabel rootSaveFolderJlabel;
+    private javax.swing.JTextField rootSaveFolderTextField;
     private javax.swing.JButton saveBtn;
     private javax.swing.JButton shippingFormBrowseBtn;
     private javax.swing.JLabel shippingFormJlabel;
     private javax.swing.JTextField shippingFormTextField;
     // End of variables declaration                   
 }
-
